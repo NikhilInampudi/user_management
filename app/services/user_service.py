9c4bf6f9a61599d8d0ca5a686034c0268b2d6c89
ninampudi <ni92@njit.edu>
2024-08-01 00:43:11 -0400

    feat(user_service): implement `update_professional_status` method

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -15,6 +15,9 @@ from uuid import UUID
 from app.services.email_service import EmailService
 from app.models.user_model import UserRole
 import logging
+from sqlalchemy import func, update, select
+from app.schemas.user_schemas import UserCreate, UserUpdate, UserResponse
+from app.dependencies import get_session
 
 settings = get_settings()
 logger = logging.getLogger(__name__)
@@ -52,30 +55,35 @@ class UserService:
     @classmethod
     async def create(cls, session: AsyncSession, user_data: Dict[str, str], email_service: EmailService) -> Optional[User]:
         try:
+            # Determine the role before validation
+            user_count = await cls.count(session)
+            user_role = UserRole.ADMIN if user_count == 0 else UserRole.ANONYMOUS
+            user_data['role'] = user_role  # Set the role based on the user count condition
+
             validated_data = UserCreate(**user_data).model_dump()
             existing_user = await cls.get_by_email(session, validated_data['email'])
             if existing_user:
                 logger.error("User with given email already exists.")
                 return None
+
             validated_data['hashed_password'] = hash_password(validated_data.pop('password'))
             new_user = User(**validated_data)
+
             new_nickname = generate_nickname()
             while await cls.get_by_nickname(session, new_nickname):
                 new_nickname = generate_nickname()
             new_user.nickname = new_nickname
-            logger.info(f"User Role: {new_user.role}")
-            user_count = await cls.count(session)
-            new_user.role = UserRole.ADMIN if user_count == 0 else UserRole.ANONYMOUS            
-            if new_user.role == UserRole.ADMIN:
-                new_user.email_verified = True
 
-            else:
-                new_user.verification_token = generate_verification_token()
-                await email_service.send_verification_email(new_user)
+            if new_user.role == UserRole.ADMIN:
+                new_user.email_verified = True  # Auto-verify admin for convenience
+
+            new_user.verification_token = generate_verification_token()
 
             session.add(new_user)
             await session.commit()
+            await email_service.send_verification_email(new_user)
             return new_user
+
         except ValidationError as e:
             logger.error(f"Validation error during user creation: {e}")
             return None
@@ -83,24 +91,29 @@ class UserService:
     @classmethod
     async def update(cls, session: AsyncSession, user_id: UUID, update_data: Dict[str, str]) -> Optional[User]:
         try:
-            # validated_data = UserUpdate(**update_data).dict(exclude_unset=True)
+            # Extract specific data and validate update payload
             validated_data = UserUpdate(**update_data).model_dump(exclude_unset=True)
-
+            # If updating password, hash it before saving
             if 'password' in validated_data:
                 validated_data['hashed_password'] = hash_password(validated_data.pop('password'))
+
+            # Apply updates to the user
             query = update(User).where(User.id == user_id).values(**validated_data).execution_options(synchronize_session="fetch")
-            await cls._execute_query(session, query)
+            await session.execute(query)
+            await session.commit()
+
+            # Fetch updated user to ensure the update was applied
             updated_user = await cls.get_by_id(session, user_id)
             if updated_user:
-                session.refresh(updated_user)  # Explicitly refresh the updated user object
-                logger.info(f"User {user_id} updated successfully.")
+                session.refresh(updated_user)  # Ensure the session has the latest data
                 return updated_user
             else:
-                logger.error(f"User {user_id} not found after update attempt.")
-            return None
-        except Exception as e:  # Broad exception handling for debugging
+                return None
+        except Exception as e:
             logger.error(f"Error during user update: {e}")
             return None
+    
+
 
     @classmethod
     async def delete(cls, session: AsyncSession, user_id: UUID) -> bool:
@@ -170,7 +183,8 @@ class UserService:
         if user and user.verification_token == token:
             user.email_verified = True
             user.verification_token = None  # Clear the token once used
-            user.role = UserRole.AUTHENTICATED
+            if user.role == UserRole.ANONYMOUS:
+                user.role = UserRole.AUTHENTICATED
             session.add(user)
             await session.commit()
             return True
@@ -199,3 +213,70 @@ class UserService:
             await session.commit()
             return True
         return False
+    
+    @classmethod
+    async def search_users(cls, session: AsyncSession, username: Optional[str] = None, email: Optional[str] = None, first_name: Optional[str] = None, last_name: Optional[str] = None, role: Optional[str] = None, account_status: Optional[str] = None, registration_date_from: Optional[datetime] = None, registration_date_to: Optional[datetime] = None, skip: int = 0, limit: int = 10) -> List[User]:
+        query = select(User)
+        filters = {
+            User.nickname: func.lower(username) if username else None,
+            User.email: func.lower(email) if email else None,
+            User.first_name: func.lower(first_name) if first_name else None,
+            User.last_name: func.lower(last_name) if last_name else None,
+            User.role: role if role else None,
+            User.is_locked: True if account_status and account_status.lower() == 'locked' else (False if account_status and account_status.lower() == 'active' else None),
+            User.created_at: (registration_date_from, registration_date_to)
+        }
+    
+        for attribute, value in filters.items():
+            if isinstance(value, tuple):  # Special handling for date range
+                query = query.filter(attribute >= value[0], attribute <= value[1]) if value[0] and value[1] else query
+            elif value is not None:
+                query = query.filter(attribute == value)
+
+        query = query.offset(skip).limit(limit)
+        result = await cls._execute_query(session, query)
+        users = result.scalars().all() if result else []
+
+    # Prepare user responses with registration date and account status
+        user_responses = [
+            UserResponse(
+                id=user.id,
+                email=user.email,
+                nickname=user.nickname,
+                is_professional=user.is_professional,
+                role=user.role,
+                registration_date=user.created_at,
+                account_status="Active" if not user.is_locked else "Locked"
+            ) for user in users
+        ]
+
+        return user_responses
+    
+    @classmethod
+    async def update_professional_status(cls, session: AsyncSession, user_id: UUID, is_professional: bool) -> Optional[User]:
+        """
+        Update the professional status of a user.
+
+        :param session: An AsyncSession instance for database access.
+        :param user_id: UUID of the user to update.
+        :param is_professional: Boolean indicating the desired professional status.
+        :return: The updated user object if successful, or None if the user was not found.
+        """
+        try:
+            # Retrieve the user by ID
+            user = await cls.get_by_id(session, user_id)
+            if not user:
+                logger.error(f"User {user_id} not found")
+                return None
+
+            # Update the professional status and set the current timestamp
+            user.update_professional_status(is_professional)
+            session.add(user)
+            await session.commit()
+
+            logger.info(f"User {user_id} professional status updated to {is_professional}")
+            return user
+        except Exception as e:
+            logger.error(f"Error updating professional status for User {user_id}: {e}")
+            await session.rollback()
+            return None
